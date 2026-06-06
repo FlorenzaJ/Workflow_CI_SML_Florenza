6ca6e5a891eb5b9b2cbec6fc3f19edbc3c65b4a7
FlorenzaJ <03081230079@student.uph.edu>
2026-06-06 10:03:19 +0700

    First commit: Setup CI/CD Pipeline

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import mlflow
+import mlflow.sklearn
+import os
+import shutil
+
+def train_basic_model():
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("CI_CD_Fraud_Detection")
+    
+    df = pd.read_csv("clean_dataset.csv")
+    X = df.drop('Fraud_Flag', axis=1).select_dtypes(include=['number']).astype(float)
+    y = df['Fraud_Flag']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    with mlflow.start_run():
+        rf = RandomForestClassifier(n_estimators=50, random_state=42)
+        rf.fit(X_train, y_train)
+        
+        mlflow.sklearn.log_model(rf, "model")
+        
+        if os.path.exists("saved_model"):
+            shutil.rmtree("saved_model")
+        mlflow.sklearn.save_model(rf, "saved_model")
+
+if __name__ == "__main__":
+    train_basic_model()
